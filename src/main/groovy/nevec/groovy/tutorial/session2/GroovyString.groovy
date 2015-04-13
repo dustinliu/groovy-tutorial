66b4a19f31f7daf71024ee970a351b257c78166b
Dustin Liu <dustinl@yahoo-inc.com>
2015-04-13 15:31:38 +0800

    add session 2

---
+++ src/main/groovy/nevec/groovy/tutorial/session2/GroovyString.groovy
@@ -0,0 +1,57 @@
+package nevec.groovy.tutorial.session2
+
+println 'Joe said "JoeJoeJoeJoeJoe"'
+
+def v = 'fddddfsd'
+println "value is $v"
+
+//what = new StringBuilder('fence')
+//text = "The cow jumped over the $what"
+//println text
+//what . replace(0, 5, "moon")
+//println text
+
+//def memo = """
+//kfld saflljkflds
+//     jklfdjls afjd
+//   fdsjkalf
+//   $v
+//"""
+//println memo
+
+//str = "It's a rainy day in Seattle"
+//println str
+//str -= "rainy "
+//println str
+
+//for (str in 'held'..'helm') {
+//    print "${str}" + " "
+//}
+//println ""
+
+
+//def pattern = ~"(G|g)roovy"
+//def text = 'Groovy is Hip'
+//if (text =~ pattern)
+//    println "match"
+//else
+//    println "no match"
+//
+//if (text ==~ pattern)
+//    println "match"
+//else
+//    println "no match"
+//
+//def matcher = 'Groovy is groovy' =~/(G|g)roovy/
+//print "Size of matcher is ${matcher.size()} "
+//println "with elements ${matcher[0]} and ${matcher[1]}."
+
+
+
+//def str = 'Groovy is groovy, really groovy'
+//println str
+//def result = (str =~ /groovy/).replaceAll('hip')
+//println result
+
+
+//collection
---
+++ src/main/groovy/nevec/groovy/tutorial/session2/pickEven.groovy
@@ -0,0 +1,11 @@
+package nevec.groovy.tutorial.session2
+
+def sum(n) {
+    def total = 0
+    for (int i = 2; i <= n; i += 2) {
+        total += i
+    }
+    total
+}
+
+//string
